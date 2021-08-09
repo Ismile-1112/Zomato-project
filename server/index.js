33fe99de5e08392c2053c05b154ca439b11895ce
Vismay Parikh <vismayparikh1112@gmail.com>
2021-08-09 19:01:07 +0530

    add statics and methods, refactor signup route

---
+++ server/index.js
@@ -0,0 +1,28 @@
+// importing Env Variables
+require("dotenv").config();
+
+// Libraries
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+// Microservice routes
+import Auth from "./API/Auth";
+
+// Database connection
+import ConnectDB from "./database/connection";
+
+const zomato = express();
+
+// Application Middlewares
+zomato.use(express.json());
+zomato.use(express.urlencoded({ extended: false }));
+zomato.use(helmet());
+zomato.use(cors());
+
+// Application Routes
+zomato.use("/auth", Auth);
+
+zomato.get("/", (req, res) => res.json({ message: "Setup Success"}));
+
+zomato.listen(4000, () => ConnectDB().then(() => console.log("Server is running")).catch(() => console.log("Server is running, but database connection failed...")));
