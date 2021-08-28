8ac1eac30a763cf3e1f644e4a90448587adab801
Vismay Parikh <vismayparikh1112@gmail.com>
2021-08-28 13:16:48 +0530

    add image uploader route

---
+++ server/API/Image/index.js
@@ -0,0 +1,48 @@
+// libraries
+import express from "express";
+import passport from "passport";
+import multer from "multer";
+
+// database model
+import { ImageModel } from "../../database/allModels";
+
+// utilities
+import { s3Upload } from "../../Utils/AWS/s3";
+
+const Router = express.Router();
+
+// Multer config
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+
+/*
+Route          /
+Description    Uploads given image to S3 bucket, and saves file link to mongodb
+Params         none
+Access         public
+Method         POST
+*/
+
+Router.post("/", upload.single("file"), async (req, res) => {
+   try{
+       const file = req.file;
+
+       // s3 bucket options
+       const bucketOptions = {
+           Bucket: "webdevproject1112",
+           Key: file.originalname,
+           Body: file.buffer,
+           ContentType: file.mimetype,
+           ACL: "public-read" // Access Control List
+       };
+
+       const uploadImage = await s3Upload(bucketOptions);
+
+       return res.status(200).json({ uploadImage });
+   }catch(error){
+       return res.status(500).json({ error: error.message });
+   }
+});
+
+export default Router;
