3c3a111233ea5702e97f39af079ca5f1213b812e
Vismay Parikh <vismayparikh1112@gmail.com>
2021-10-13 08:50:57 +0530

    add menu and photos

--- client/src/Page/Restaurant/Photos.jsx
+++ client/src/Page/Restaurant/Photos.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
-import ImageViewer from 'react-simple-image-viewer';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import ImageViewer from "react-simple-image-viewer";
 
 // component
 import PhotoCollection from '../../Components/restaurant/PhotosCollection';
 
+// redux actions
+import { getImage } from "../../Redux/Reducer/Image/Image.action";
+
 const Photos = () => {
-    const [photos, setPhotos] = useState([
-        "https://b.zmtcdn.com/data/pictures/chains/1/110271/cb9ac8fa88a31867fa96e091581daf0b.jpg?",
-        "https://b.zmtcdn.com/data/pictures/chains/1/110271/fe7141486fdb8b7ec00a23981c07c34a.jpg?",
-        "https://b.zmtcdn.com/data/pictures/chains/1/110271/3df89cb2491e227b77a719492b33ef44.jpg?"
-    ]);
+    const [photos, setPhotos] = useState([]);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [CurrentImg, setCurrentImg] = useState(0);
+
+    const reduxState = useSelector((globalStore) => globalStore.restaurant.selectedRestaurant.restaurant);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (reduxState) {
+            dispatch(getImage(reduxState?.photos)).then((data) => {
+                const images = [];
+                data.payload.image.images.map(({ location }) => images.push(location));
+                setPhotos(images);
+            });
+        }
+    }, []);
     const closeViewer = () => setIsMenuOpen(false);
     const openViewer = () => setIsMenuOpen(true);
 
